4919c8972f5611f7707b946b45c1b9b03906e975
kirillstankevich2005-wq <kirillstankevich2005@gmail.com>
2025-09-11 22:07:25 +0300

    Update realt.py

--- lib/service/realt.py
+++ lib/service/realt.py
@@ -43,7 +43,7 @@ def fetch_ads():
             "variables": {
                 "data": {   
                     "where": {
-                        "rooms": ["1,2,3"],
+                        "rooms": ["1","2","3"],
                         "priceTo": "600",
                         "priceType": "840",
                         "addressV2": [{"townUuid": "4cb07174-7b00-11eb-8943-0cc47adabd66"}],
@@ -138,3 +138,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
